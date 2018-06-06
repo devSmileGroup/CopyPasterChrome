3123eb98a3d269d4761521c240cd0c7f3bbdc9b2
Aleksandr Asimov <grynchuk.aeksandr0@gmail.com>
2018-06-06 17:35:12 +0300

    Add EVENT BDLCLICK and some else

--- background.js
+++ background.js
@@ -1,7 +1,17 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 chrome.commands.onCommand.addListener(function (command) {
     if (command == "toggle-pin") {
-        chrome.storage.local.get(['key'], function (result) {
-            alert('Name: ' + result.key.name + '\nProfile: ' + result.key.profile + '\nEmail: ' + result.key.email + '\nContactDate' + result.key.contactDate)
-        });
+        alert('must called function')
+        // writefc();
+
+        // chrome.storage.local.get(['key'], function (result) {
+        //     document.getElementById("u_0_p").value = result.key.name
+        //     document.getElementById("u_0_r").value = result.key.profile
+        //     document.getElementById("u_0_u").value = result.key.email
+        //     document.getElementById("u_0_11").value = result.key.contactDate
+        // });
     }
 });
