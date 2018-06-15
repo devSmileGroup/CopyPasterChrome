c27ea2c4947046dcbdcd1bcdf3c8ced10c3ab345
Aleksandr Asimov <grynchuk.aeksandr0@gmail.com>
2018-06-15 15:07:00 +0300

    add css and instruction and shortkey

--- cleverstaff.js
+++ cleverstaff.js
@@ -1,24 +1,25 @@
-addEventListener('dblclick', function () {
-    // alert('must called function')
+addEventListener("keydown", event => {
+    if (event.key == "q" && event.altKey) {
+        // alert('must called function')
 
 
-    chrome.storage.local.get(['linkedinfo'], function (result) {
-        console.log(result)
-        document.getElementsByClassName("item")[1].children[1].value = result.linkedinfo.name
-        document.getElementsByClassName("item")[18].children[1].value = result.linkedinfo.email
-        document.getElementsByClassName("item")[20].children[1].value = result.linkedinfo.profile
-        document.getElementsByClassName("item")[4].children[1].value = result.linkedinfo.education.university
-        document.getElementsByClassName("item")[13].children[1].value = result.linkedinfo.jobs[0].firm
-        document.getElementsByClassName("item")[14].children[1].value = result.linkedinfo.jobs[0].post
-    });
-    chrome.storage.local.get(['linkedincfo'], function (result) {
-        console.log(result)
-        document.getElementsByClassName("item")[18].children[1].value = result.linkedincfo.email
-        document.getElementsByClassName("item")[19].children[1].value = result.linkedincfo.skype
-        document.getElementsByClassName("item")[20].children[1].value = result.linkedincfo.profile
-        document.getElementsByClassName("item")[24].children[1].value = result.linkedincfo.github
+        chrome.storage.local.get(['linkedinfo'], function (result) {
+            console.log(result)
+            document.getElementsByClassName("item")[1].children[1].value = result.linkedinfo.name
+            document.getElementsByClassName("item")[18].children[1].value = result.linkedinfo.email
+            document.getElementsByClassName("item")[20].children[1].value = result.linkedinfo.profile
+            document.getElementsByClassName("item")[4].children[1].value = result.linkedinfo.education.university
+            document.getElementsByClassName("item")[13].children[1].value = result.linkedinfo.jobs[0].firm
+            document.getElementsByClassName("item")[14].children[1].value = result.linkedinfo.jobs[0].post
+        });
+        chrome.storage.local.get(['linkedincfo'], function (result) {
+            console.log(result)
+            document.getElementsByClassName("item")[18].children[1].value = result.linkedincfo.email
+            document.getElementsByClassName("item")[19].children[1].value = result.linkedincfo.skype
+            document.getElementsByClassName("item")[20].children[1].value = result.linkedincfo.profile
+            document.getElementsByClassName("item")[24].children[1].value = result.linkedincfo.github
 
-    })
-
+        })
+    }
 
 })
--- linkedinc.js
+++ linkedinc.js
@@ -8,23 +8,25 @@ var contact = {
 
 
 
-addEventListener('dblclick', function () {
-    contact.profile = document.getElementsByClassName("ci-vanity-url")[0].children[2].children[0].innerText
-    if (document.getElementsByClassName("ci-email").length != 0) {
-        contact.email = document.getElementsByClassName("ci-email")[0].children[2].children[0].innerText
+addEventListener("keydown", event => {
+    if (event.key == "q" && event.altKey) {
+        contact.profile = document.getElementsByClassName("ci-vanity-url")[0].children[2].children[0].innerText
+        if (document.getElementsByClassName("ci-email").length != 0) {
+            contact.email = document.getElementsByClassName("ci-email")[0].children[2].children[0].innerText
+        }
+        if (document.getElementsByClassName("ci-websites").length != 0) {
+            if (document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[1].innerText === '(GitHub)')
+                contact.github = document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[0].innerText
+        }
+        if (document.getElementsByClassName("ci-ims").length != 0) {
+            if (document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[1].innerText === '(Skype)')
+                contact.skype = document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[0].innerText
+        }
+        chrome.storage.local.set({
+            linkedincfo: contact
+        }, function () {
+            console.log(contact)
+            alert('Done!');
+        });
     }
-    if (document.getElementsByClassName("ci-websites").length != 0) {
-        if (document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[1].innerText === '(GitHub)')
-            contact.github = document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[0].innerText
-    }
-    if (document.getElementsByClassName("ci-ims").length != 0) {
-        if (document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[1].innerText === '(Skype)')
-            contact.skype = document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[0].innerText
-    }
-    chrome.storage.local.set({
-        linkedincfo: contact
-    }, function () {
-        console.log(contact)
-        alert('Done!');
-    });
 })
