6698d35f1ff655fcf6623e13a1b964bdcea5b21b
Aleksandr Asimov <grynchuk.aeksandr0@gmail.com>
2018-06-19 12:33:37 +0300

    some changes

--- linkedin.js
+++ linkedin.js
@@ -38,70 +38,70 @@ var person = {
 
 addEventListener("keydown", event => {
     if (event.key == "q" && event.altKey) {
-        // try {
-        // scrollEnd();
-        /////click  skill section
         try {
-            document.getElementsByClassName("pv-skill-categories-section")[0].children[2].children[0].click()
-        } catch (err) {}
-        // for (i = 0; i < document.getElementsByClassName("pv-accomplishments-block__expand").length; i++) {
-        //     document.getElementsByClassName("pv-accomplishments-block__expand")[i].click()
-        // }
-        /////click  language+ section
-        try {
-            document.getElementsByClassName("pv-accomplishments-block__expand")[0].click()
-        } catch (err) {}
+            // scrollEnd();
+            /////click  skill section
+            try {
+                document.getElementsByClassName("pv-skill-categories-section")[0].children[2].children[0].click()
+            } catch (err) {}
+            // for (i = 0; i < document.getElementsByClassName("pv-accomplishments-block__expand").length; i++) {
+            //     document.getElementsByClassName("pv-accomplishments-block__expand")[i].click()
+            // }
+            /////click  language+ section
+            try {
+                document.getElementsByClassName("pv-accomplishments-block__expand")[0].click()
+            } catch (err) {}
 
-        person.name = document.getElementsByClassName("pv-top-card-section__name")[0].outerText
-        person.post = document.getElementsByClassName("pv-top-card-section__headline")[0].outerText
-        person.country = document.getElementsByClassName("pv-top-card-section__location")[0].outerText
-        try {
-            person.jobs[0].post = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[0].outerText
-            person.jobs[0].firm = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[1].children[1].outerText
-            person.jobs[0].date = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[2].children[1].outerText
-            person.jobs[0].time = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[3].children[1].outerText
+            person.name = document.getElementsByClassName("pv-top-card-section__name")[0].outerText
+            person.post = document.getElementsByClassName("pv-top-card-section__headline")[0].outerText
+            person.country = document.getElementsByClassName("pv-top-card-section__location")[0].outerText
+            try {
+                person.jobs[0].post = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[0].outerText
+                person.jobs[0].firm = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[1].children[1].outerText
+                person.jobs[0].date = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[2].children[1].outerText
+                person.jobs[0].time = document.getElementById("experience-section").getElementsByClassName("pv-entity__summary-info")[0].children[3].children[1].outerText
 
-        } catch (error) {
+            } catch (error) {
 
+            }
+            for (var i; i < document.getElementById("experience-section").children[1].children.length; i++) {
+                person.jobs[i].time = document.getElementById("experience-section").children[1].children[i].children[0].children[1].children[3].children[1].outerText
+
+            }
+            try {
+                person.education.university = document.getElementsByClassName("pv-education-entity")[0].children[0].children[1].children[0].children[0].outerText
+            } catch (err) {}
+            // person.skills[0] = document.getElementsByClassName("pv-skill-categories-section__top-skills")[0].children[0].children[0].children[1].outerText
+            // person.skills[1] = document.getElementsByClassName("pv-skill-categories-section__top-skills")[0].children[1].children[0].children[1].outerText
+            // person.skills[2] = document.getElementsByClassName("pv-skill-categories-section__top-skills")[0].children[2].children[0].children[1].outerText
+            for (var i = 0; i < document.getElementsByClassName("pv-skill-category-entity__name").length; i++) {
+                person.skills[i] = document.getElementsByClassName("pv-skill-category-entity__name")[i].outerText
+            }
+            for (var i = 0; i < person.skills.length; i++) {
+                person.aSkills = person.aSkills + ' ● ' + person.skills[i];
+            }
+            ////////////////////********JOBS**********///////////////////
+            // var length = document.getElementById("experience-section").children[1].children.length
+            // for (var i = 0; i < 3; i++) {
+            //     person.jobs[i].post = document.getElementById("experience-section").children[1].children[i].children[0].children[1].children[0].textContent
+
+            // }
+            ////////////////////********JOBS**********///////////////////
+            // person.profile = document.getElementsByClassName('pv-contact-info__ci-container')[0].innerText
+            // person.email = document.getElementsByClassName('pv-contact-info__ci-container')[1].innerText
+            // person.contactDate = document.getElementsByClassName('pv-contact-info__ci-container')[2].innerText
+            // console.log(person)
+
+            chrome.storage.local.set({
+                linkedinfo: person
+            }, function () {
+                console.log(person)
+                alert('Person ' + person.name + ' save!');
+            });
+        } catch {
+            alert("Please reload user page or send me this user page link")
         }
-        for (var i; i < document.getElementById("experience-section").children[1].children.length; i++) {
-            person.jobs[i].time = document.getElementById("experience-section").children[1].children[i].children[0].children[1].children[3].children[1].outerText
-
-        }
-        try {
-            person.education.university = document.getElementsByClassName("pv-education-entity")[0].children[0].children[1].children[0].children[0].outerText
-        } catch (err) {}
-        // person.skills[0] = document.getElementsByClassName("pv-skill-categories-section__top-skills")[0].children[0].children[0].children[1].outerText
-        // person.skills[1] = document.getElementsByClassName("pv-skill-categories-section__top-skills")[0].children[1].children[0].children[1].outerText
-        // person.skills[2] = document.getElementsByClassName("pv-skill-categories-section__top-skills")[0].children[2].children[0].children[1].outerText
-        for (var i = 0; i < document.getElementsByClassName("pv-skill-category-entity__name").length; i++) {
-            person.skills[i] = document.getElementsByClassName("pv-skill-category-entity__name")[i].outerText
-        }
-        for (var i = 0; i < person.skills.length; i++) {
-            person.aSkills = person.aSkills + ' ● ' + person.skills[i];
-        }
-        ////////////////////********JOBS**********///////////////////
-        // var length = document.getElementById("experience-section").children[1].children.length
-        // for (var i = 0; i < 3; i++) {
-        //     person.jobs[i].post = document.getElementById("experience-section").children[1].children[i].children[0].children[1].children[0].textContent
-
-        // }
-        ////////////////////********JOBS**********///////////////////
-        // person.profile = document.getElementsByClassName('pv-contact-info__ci-container')[0].innerText
-        // person.email = document.getElementsByClassName('pv-contact-info__ci-container')[1].innerText
-        // person.contactDate = document.getElementsByClassName('pv-contact-info__ci-container')[2].innerText
-        // console.log(person)
-
-        chrome.storage.local.set({
-            linkedinfo: person
-        }, function () {
-            console.log(person)
-            alert('Person ' + person.name + ' save!');
-        });
-        // } catch {
-        //     alert("Please try again or send me this user page link")
-        // }
-        // window.location.href = window.location.href + "detail/contact-info/"
+        window.location.href = window.location.href + "detail/contact-info/"
         // console.log(person)
     }
 })
--- linkedinc.js
+++ linkedinc.js
@@ -10,23 +10,27 @@ var contact = {
 
 addEventListener("keydown", event => {
     if (event.key == "q" && event.altKey) {
-        contact.profile = document.getElementsByClassName("ci-vanity-url")[0].children[2].children[0].innerText
-        if (document.getElementsByClassName("ci-email").length != 0) {
-            contact.email = document.getElementsByClassName("ci-email")[0].children[2].children[0].innerText
+        try {
+            contact.profile = document.getElementsByClassName("ci-vanity-url")[0].children[2].children[0].innerText
+            if (document.getElementsByClassName("ci-email").length != 0) {
+                contact.email = document.getElementsByClassName("ci-email")[0].children[2].children[0].innerText
+            }
+            if (document.getElementsByClassName("ci-websites").length != 0) {
+                if (document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[1].innerText === '(GitHub)')
+                    contact.github = document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[0].innerText
+            }
+            if (document.getElementsByClassName("ci-ims").length != 0) {
+                if (document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[1].innerText === '(Skype)')
+                    contact.skype = document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[0].innerText
+            }
+            chrome.storage.local.set({
+                linkedincfo: contact
+            }, function () {
+                console.log(contact)
+                alert('Done!');
+            });
+        } catch (err) {
+            alert("Please reload user page or send me this user page link")
         }
-        if (document.getElementsByClassName("ci-websites").length != 0) {
-            if (document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[1].innerText === '(GitHub)')
-                contact.github = document.getElementsByClassName("ci-websites")[0].children[2].children[0].children[0].children[0].innerText
-        }
-        if (document.getElementsByClassName("ci-ims").length != 0) {
-            if (document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[1].innerText === '(Skype)')
-                contact.skype = document.getElementsByClassName("ci-ims")[0].children[2].children[0].children[0].innerText
-        }
-        chrome.storage.local.set({
-            linkedincfo: contact
-        }, function () {
-            console.log(contact)
-            alert('Done!');
-        });
     }
 })
